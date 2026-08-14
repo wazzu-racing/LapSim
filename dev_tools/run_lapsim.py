5bee757f6d761a486d9337fe1c125ed091850843
Jacob McKee <jacobmckee06@gmail.com>
2026-08-14 16:11:23 -0700

    Improve run_lapsim.py

--- dev_tools/run_lapsim.py
+++ dev_tools/run_lapsim.py
@@ -1,4 +1,9 @@
+import tkinter
+
 import math
+from matplotlib.backends._backend_tk import NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 from gen_lapsim.spline_track import track, curve
 from models.car_model import car
@@ -10,10 +15,87 @@ class Track_Examine:
     def __init__(self, track_txt_path=""):
         self.racecar = car()
 
+        # self.run_endurance(self.racecar, 2500)
+        # self.plot_forces("RO")
         # self.run_skidpad(self.racecar, 1000)
-        # self.run_autocross(self.racecar, 1000)
-        # self.run_accel(self.racecar, 100)
+        # self.run_autocross(self.racecar, 5000)
+        # self.run_accel(self.racecar, 5000)
         # self.racecar.plot_forces()
+
+    def run_accel(self, rcar, node_count, again=False):
+        if not again:
+            self.points_x = [0, 0]
+            self.points_y = [0, 2952]
+            self.points_x2 = [100, 100]
+            self.points_y2 = [0, 2952]
+
+            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, rcar, loop=False)
+
+        return self.trk.run_sim(rcar, nodes=node_count, end_vel=2000)
+
+    def run_endurance(self, car, node_count, again=False):
+        if not again:
+            self.parse_text_to_track_pkl("/Users/jacobmckee/Documents/Wazzu_Racing/Vehicle_Dynamics/Repos/LapSim_Main/config_data/track_points/Points for Endurance.rtf")
+
+            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=True)
+            self.trk.adjust_track([40, 30, 30, 80], [100, 30, 10, 5])
+
+        return self.trk.run_sim(car, nodes=node_count)
+
+    def run_autocross(self, car, node_count, again=False):
+        if not again:
+            self.parse_text_to_track_pkl("/Users/jacobmckee/Documents/Wazzu_Racing/Vehicle_Dynamics/Repos/LapSim_Main/config_data/track_points/Auto_Points_25.rtf")
+
+            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=True)
+            self.trk.adjust_track([40, 30, 30, 80], [100, 30, 10, 5])
+
+        # self.trk.plot_without_UI()
+        return self.trk.run_sim(car, nodes=node_count)
+
+    def run_skidpad(self, car, node_count, again=False):
+        if not again:
+            self.points_x, self.points_y, self.points_x2, self.points_y2 = [], [], [], []
+
+            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=True)
+
+            self.trk.rad = [300.23622+self.racecar.t_f/2]
+            self.trk.lens = [2 * math.pi * self.trk.rad[0]]
+            self.trk.turn_dirs = [curve.Turn.RIGHT]
+
+        max_vel = math.sqrt(self.racecar.max_corner*0.99*386.0886*self.trk.rad[0])
+        return self.trk.run_sim(car, nodes=node_count, start_vel=max_vel, end_vel=max_vel, clear_arrs=False)
+
+    def plot_forces(self, tire="FO", x=True, y=True, z=True):
+        tk = tkinter.Tk()
+        fig = Figure(figsize=(10, 10), dpi=100)
+        ax = fig.add_subplot(111)
+        canvas = FigureCanvasTkAgg(fig, tk)
+        canvas.draw()
+        toolbar = NavigationToolbar2Tk(canvas, tk)
+        canvas.get_tk_widget().pack()
+        toolbar.update()
+        leg = []
+        if x:
+            leg.append('FX')
+            ax.plot(self.trk.sim.lapsim_data_storage.FO_FX_array if tire == "FO" else
+                    self.trk.sim.lapsim_data_storage.FI_FX_array if tire == "FI" else
+                    self.trk.sim.lapsim_data_storage.RO_FX_array if tire == "RO" else
+                    self.trk.sim.lapsim_data_storage.RI_FX_array)
+        if y:
+            leg.append('FY')
+            ax.plot(self.trk.sim.lapsim_data_storage.FO_FY_array if tire == "FO" else
+                    self.trk.sim.lapsim_data_storage.FI_FY_array if tire == "FI" else
+                    self.trk.sim.lapsim_data_storage.RO_FY_array if tire == "RO" else
+                    self.trk.sim.lapsim_data_storage.RI_FY_array)
+        if z:
+            leg.append('FZ')
+            ax.plot(self.trk.sim.lapsim_data_storage.FO_load_array if tire == "FO" else
+                    self.trk.sim.lapsim_data_storage.FI_load_array if tire == "FI" else
+                    self.trk.sim.lapsim_data_storage.RO_load_array if tire == "RO" else
+                    self.trk.sim.lapsim_data_storage.RI_load_array)
+        ax.legend(leg)
+        ax.grid()
+        tk.mainloop()
 
     def parse_text_to_track_pkl(self, txt_path):
         """
@@ -56,71 +138,5 @@ class Track_Examine:
         self.points_y = points_arr[1].astype(float)
         self.points_x2 = points_arr[2].astype(float)
         self.points_y2 = points_arr[3].astype(float)
-
-    # # 73 weight
-    # points_x = [0, 288, 528, 720, 912, 1104, 1296, 2016, 2256, 2016, 1776, 2016, 2256, 2016, 0, -108, 0, 108, 0, -108]
-    # points_y = [0, 36, 108, 36, 0, 36, 108, 0, 240, 480, 240, 0, 240, 480, 444, 276, 0, 276, 444, 276]
-    # points_x2 = [0, 288, 528, 720, 912, 1104, 1296, 2016, 2364, 2016, 1668, 2016, 2364, 2016, 0, -216, 0, 216, 0, -216]
-    # points_y2 = [108, 144, 216, 144, 108, 144, 216, -108, 240, 588, 240, -108, 240, 588, 552, 276, 108, 276, 552, 276]
-
-    # # 92 weight
-    # points_x = [0, 0, -108, 324, 756, 972, 1620, 2268, 1728, 1080, 1512, 2268, 1728, 1188, 540]
-    # points_y = [0, -384, -768, -1152, -1152, -1584, -1800, -1584, -1368, -960, -528, -312, 0, 432, 432]
-    # points_x2 = [216, 216, 108, 324, 756, 1188, 1620, 2052, 1728, 1296, 1512, 2484, 1512, 1188, 540]
-    # points_y2 = [0, -384, -768, -1368, -1368, -1584, -2016, -1584, -1152, -960, -744, -312, 0, 648, 648]
-
-    def run_accel(self, car, node_count, again=False):
-        if not again:
-            self.points_x = [0, 0]
-            self.points_y = [0, 2952]
-            self.points_x2 = [100, 100]
-            self.points_y2 = [0, 2952]
-
-            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=False)
-
-        return self.trk.run_sim(car, nodes=node_count, end_vel=2000)
-
-    def run_endurance(self, car, node_count, again=False):
-        if not again:
-            self.parse_text_to_track_pkl("/Users/jacobmckee/Documents/Wazzu_Racing/Vehicle_Dynamics/Repos/LapSim_Main/config_data/track_points/Points for Endurance.rtf")
-
-            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=True)
-            self.trk.adjust_track([40, 30, 30, 80], [100, 30, 10, 5])
-
-        return self.trk.run_sim(car, nodes=node_count)
-
-    def run_autocross(self, car, node_count, again=False):
-        if not again:
-            self.parse_text_to_track_pkl("/Users/jacobmckee/Documents/Wazzu_Racing/Vehicle_Dynamics/Repos/LapSim_Main/config_data/track_points/Auto_Points_25.rtf")
-
-            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=True)
-            self.trk.adjust_track([40, 30, 30, 80], [100, 30, 10, 5])
-
-        # self.trk.plot_without_UI()
-        return self.trk.run_sim(car, nodes=node_count)
-
-    def run_skidpad(self, car, node_count, again=False):
-        if not again:
-            self.points_x, self.points_y, self.points_x2, self.points_y2 = [], [], [], []
-
-            self.trk = track(self.points_x, self.points_y, self.points_x2, self.points_y2, car, loop=True)
-
-            self.trk.rad = [300.23622+self.racecar.t_f/2]
-            self.trk.lens = [2 * math.pi * self.trk.rad[0]]
-            self.trk.turn_dirs = [curve.Turn.RIGHT]
-
-        max_vel = math.sqrt(self.racecar.max_corner*0.99*386.0886*self.trk.rad[0])
-        return self.trk.run_sim(car, nodes=1000, start_vel=max_vel, end_vel=max_vel, clear_arrs=False)
-
-    # def run(self):
-    #     for i in np.linspace(6, 16, 11):
-    #         self.racecar.h = float(i)
-    #         self.racecar.recalculate_characteristics()
-    #         self.run_constant_velocity_skidpad()
-    #
-    #     plt.plot(np.linspace(6, 16, 11), self.times)
-    #     plt.ylabel("Time (s)")
-    #     plt.xlabel("COG (inches)")
-    #     plt.show()
 
 examine = Track_Examine()
